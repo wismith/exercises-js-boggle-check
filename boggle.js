c8fbda4bc45327c772137ba2cdd5491285533ae5
Will Smith <wismith@davidson.edu>
2020-02-03 16:07:30 -0800

    edits to randomize dice

--- boggle.js
+++ boggle.js
@@ -16,6 +16,27 @@ function printBoggleBoard(boggleBoard) {
   }
 }
 
+
+// Function to shuffle array, courtesy of Lily
+function shuffle(array) {
+  var currentIndex = array.length; // temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 /**
  * Shake a boggle board and fill it with letters.
  */
@@ -40,10 +61,12 @@ function shake(boggleBoard) {
     'DEILRX',
   ];
 
+  let randomDice = shuffle(dice);
+
   for (let j = 0; j < boggleBoard.length; j++) {
     for (let i = 0; i < boggleBoard[j].length; i++) {
       // NOTE: I use splice below to prevent repetition of die on the board
-      let die = dice.splice(Math.floor(Math.random() * dice.length),1)[0];
+      let die = randomDice.pop();
       // NOTE: Don't need slice here, bc only assigning one of the sides of the die to be 'up'
       let char = die[Math.floor(Math.random() * 6)];
       boggleBoard[j][i] = char;
