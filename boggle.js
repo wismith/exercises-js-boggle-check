924793e695d082ee6c8db392241a8c72c96f7dd2
Will Smith <wismith@davidson.edu>
2020-02-04 14:05:22 -0800

    boggle checks: vert, horiz, diag

--- boggle.js
+++ boggle.js
@@ -73,7 +73,7 @@ function shuffle(array) {
 /**
  * Shake a boggle board and fill it with letters.
  */
-function shake(boggleBoard) {
+function shake(board) {
   // Create array of 6-sided dice
   let dice = [
     'AAEEGN',
@@ -96,13 +96,13 @@ function shake(boggleBoard) {
 
   let randomDice = shuffle(dice);
 
-  for (let i = 0; i < boggleBoard.length; i++) {
-    for (let j = 0; j < boggleBoard[i].length; j++) {
-      boggleBoard[i][j] = randomDice[i * board.length + j][Math.floor(Math.random() * dice[0].length)];
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j] = randomDice[i * board.length + j][Math.floor(Math.random() * dice[0].length)];
     }
   }
 
-  return boggleBoard;
+  return board;
 }
 
 function boggleBoardContainsWord(board, word) {
@@ -124,28 +124,83 @@ function boggleBoardContainsWord(board, word) {
     return false;
   }
 
-  for (let i = 0; i < board.length; i++) {
-    if (board[i].join('').includes(word) || board[i].reverse().join('').includes(word)) {
+  function rowContainsWord(i) {
+    if (board[i].join('').includes(word) || board[i].reverse().includes(word)) {
       return true;
     }
   }
-  let columns = [];
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      let column = [];
-      column.push(board[j][i]);
+
+  function columnContainsWord(j) {
+    let column = [];
+    for (let i = 0; i < board.length; i++) {
+      column.push(board[i][j]);
     }
-  }
-  for (let column of columns) {
-    if (column.join('').includes(word) || column.reverse().join('').includes(word)) {
+    if (column.join('').includes(word) || column.reverse().includes(word)) {
       return true;
     }
   }
+
+  function checkDiagonals() {
+    let diagonals = [];
+    function diagonalConstructor(listI, listJ) {
+      let diagonal = [];
+      for (let i = 0; i < listI.length; i++) {
+        diagonal.push(board[listI[i]][listJ[i]]);
+      }
+      return diagonal;
+    }
+    for (let [x, y] of [
+      [[0], [0]],
+      [[1, 0], [0, 1]],
+      [[2, 1, 0], [0, 1, 2]],
+      [[3, 2, 1, 0], [0, 1, 2, 3]],
+      [[3, 2, 1], [1, 2, 3]],
+      [[3, 2], [2, 3]],
+      [[3], [3]],
+      [[0], [3]],
+      [[0, 1], [2, 3]],
+      [[0, 1, 2], [1, 2, 3]],
+      [[0, 1, 2, 3], [0, 1, 2, 3]],
+      [[1, 2, 3], [0, 1, 2]],
+      [[2, 3], [0, 1]],
+      [[3], [0]],
+    ]) {
+      diagonals.push(diagonalConstructor(x, y));
+    }
+
+    for (let diagonal of diagonals) {
+      if (diagonal.join('').includes(word) || diagonal.reverse().join('').includes(word)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Check Rows
+  for (let i = 0; i < board.length; i++) {
+    if (rowContainsWord(i)) {
+      return true;
+    };
+  }
+  // Check columns
+  for (let j = 0; j < board.length; j++) {
+    if (columnContainsWord(j)) {
+      return true;
+    }
+  }
+  // Check Diagonals
+  if (checkDiagonals()) {
+    return true;
+  };
+
   return false;
 }
 
-let board = newBoggleBoard();
+if (require.main === module) {
+  let board = newBoggleBoard();
 
-shake(board);
+  shake(board);
 
-printBoggleBoard(board);
+  printBoggleBoard(board);
+  console.log(boggleBoardContainsWord(board, 'IS'));
+}
